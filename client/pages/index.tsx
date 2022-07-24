947838b593a0d8d7d4e65bd23369e38d16ff4ffd
Charles <47355793+MeatBoyed@users.noreply.github.com>
2022-07-24 18:29:19 +0200

    feat: Changed car/[slug] to SSG to handle 404 errors

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,40 +2,30 @@ import { Container } from '@mui/material';
 import type { GetStaticProps, NextPage } from 'next';
 // import styles from '../styles/Home.module.css';
 
-import {
-	BannerSection,
-	CategoriesSection,
-	AboutSection,
-	ViewMoreSection,
-} from '../Lib/HomePage';
+import { BannerSection, CategoriesSection, AboutSection, ViewMoreSection } from '../Lib/HomePage';
 
 import { SanityClient } from '../Utils';
 import { ViewMoreSectionData } from '../Utils/Interfaces';
 
 const Home: NextPage<ViewMoreSectionData> = ({ data }) => {
-	return (
-		<Container
-			id="HomePage"
-			component="section"
-			maxWidth={false}
-			disableGutters={true}
-		>
-			<BannerSection />
-			<CategoriesSection />
-			<AboutSection />
-			<ViewMoreSection data={data} />
-		</Container>
-	);
+  return (
+    <Container id="HomePage" component="section" maxWidth={false} disableGutters={true}>
+      <BannerSection />
+      <CategoriesSection />
+      <AboutSection />
+      <ViewMoreSection data={data} />
+    </Container>
+  );
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-	const query =
-		'*[_type == "product"]{slug,title,price,make,series,model,registrationYear,milage,transmission,fuelType,thumbnail}';
-	const res: [ViewMoreSectionData] = await SanityClient.fetch(query);
+  const query =
+    '*[_type == "product"]{slug,title,price,make,series,model,registrationYear,milage,transmission,fuelType,thumbnail}';
+  const res: [ViewMoreSectionData] = await SanityClient.fetch(query);
 
-	return {
-		props: { data: res },
-	};
+  return {
+    props: { data: res },
+  };
 };
 
 export default Home;
