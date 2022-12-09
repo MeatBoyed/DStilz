72be035a9739f5d9b0465edadfe263f1896282e4
Charles Ross <charlesrossouw@outlook.com>
2022-12-09 10:01:55 +0200

    feat: InfoCard created with Rendere

---
+++ client/Utils/Components/InfoCard.tsx
@@ -0,0 +1,41 @@
+import type { NextPage } from 'next';
+
+import Image from 'next/image';
+
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+
+import { InfoCardProps } from '../Interfaces';
+
+export const InfoCard: NextPage<InfoCardProps> = ({ icon, header, body }) => {
+	return (
+		<Grid
+			container
+			direction="column"
+			justifyContent="center"
+			alignItems="center"
+		>
+			<Grid item width={45}>
+				<Image
+					src={icon}
+					width={45}
+					height={45}
+					objectFit="fill"
+					alt="Sell Cars Icon"
+				/>
+			</Grid>
+			<Grid item>
+				<Typography variant="h5" align="center" sx={{ fontSize: '1em' }}>
+					{header}
+				</Typography>
+			</Grid>
+			<Grid item width="80%">
+				<Typography align="center" sx={{ fontSize: '.7em' }}>
+					{body}
+				</Typography>
+			</Grid>
+		</Grid>
+	);
+};
+
+export default InfoCard;
---
+++ client/Utils/Components/InfoCardRender.tsx
@@ -0,0 +1,39 @@
+import type { NextPage } from 'next';
+
+import Grid from '@mui/material/Grid';
+
+import CarSale from '../../public/CarSales.png';
+import { InfoCard } from '..';
+
+export const InfoCardRender: NextPage = () => {
+	return (
+		<Grid
+			container
+			direction="column"
+			justifyContent="center"
+			alignItems="center"
+			spacing={4}
+		>
+			<Grid item>
+				<InfoCard
+					icon={CarSale.src}
+					header={'We Buy Cars Cash'}
+					body={
+						'Reach South Africa’s biggest and most serious audience of car buyers.'
+					}
+				/>
+			</Grid>
+			<Grid item>
+				<InfoCard
+					icon={CarSale.src}
+					header={'We Buy Cars Cash'}
+					body={
+						'Search through 61 cars online, and have it delivered to your door.'
+					}
+				/>
+			</Grid>
+		</Grid>
+	);
+};
+
+export default InfoCardRender;
