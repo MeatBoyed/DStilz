42a788ef3ef3220bfbf0a90423f0e269cb16757d
Charles Ross <charlesrossouw@outlook.com>
2023-01-07 16:06:22 +0200

    fix: Specified prisma globally for production

--- client/DAS/prisma.ts
+++ client/DAS/prisma.ts
@@ -1,33 +1,36 @@
 import { PrismaClient } from '@prisma/client';
 
-// declare global {
-// 	namespace NodeJS {
-// 		interface Global {
-// 			prisma: PrismaClient;
-// 		}
-// 	}
-// }
+// FOR PRODUCTION
+declare global {
+	namespace NodeJS {
+		interface Global {
+			prisma: PrismaClient;
+		}
+	}
+}
 
-// export let prisma: PrismaClient;
+export let prisma: PrismaClient;
 
-// if (typeof window === 'undefined') {
-// 	if (process.env.NODE_ENV === 'production') {
-// 		prisma = new PrismaClient();
-// 	} else {
-// 		if (!global.prisma) {
-// 			global.prisma = new PrismaClient();
-// 		}
+if (typeof window === 'undefined') {
+	if (process.env.NODE_ENV === 'production') {
+		prisma = new PrismaClient();
+	}
+	// } else {
+	// 	if (!global.prisma) {
+	// 		global.prisma = new PrismaClient();
+	// 	}
 
-// 		prisma = global.prisma;
-// 	}
-// }
+	// 	prisma = global.prisma;
+	// }
+}
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+// FOR DEV PURPOSES
+// const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
-	globalForPrisma.prisma ||
-	new PrismaClient({
-		log: ['query'],
-	});
+// export const prisma =
+// 	globalForPrisma.prisma ||
+// 	new PrismaClient({
+// 		log: ['query'],
+// 	});
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
